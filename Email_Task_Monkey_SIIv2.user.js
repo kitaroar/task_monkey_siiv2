06d106bd1469aa414312ffe3c5d0c7fd68ad06a7
Daniel Ghita <31425662+kitaroar@users.noreply.github.com>
2024-09-19 04:11:01 +0300

    Update Email_Task_Monkey_SIIv2.user.js

--- Email_Task_Monkey_SIIv2.user.js
+++ Email_Task_Monkey_SIIv2.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Trimitere Task si Email
 // @namespace    http://tampermonkey.net/
-// @version      2.8
+// @version      3.0
 // @description  Adaugă un buton și un formular pe pagina web care trimite date către Google Apps Script
 // @author       ORCT_AR
 // @match        *://rc-prod.onrc.sii/*
@@ -25,7 +25,9 @@
     let judr = '';
     let operator = '';
     let judo = '';
+    let judcerere = '';
     let firma = '';
+    let mailJudet = '';
     let formData = ''; // Will store form data (checkboxes and message)
 
     // Creăm un buton pe pagina web
@@ -75,7 +77,9 @@
                 columnIndexes.numarInregistrare = index;
             } else if (headerText.includes('Dată înregistrare')) {
                 columnIndexes.dataInregistrare = index;
-            } else if (headerText.includes('Registrator')) {
+            } else if (headerText.includes('Judet cerere')) {
+                columnIndexes.judcerere = index;
+            }else if (headerText.includes('Registrator')) {
                 columnIndexes.registrator = index;
             } else if (headerText.includes('Judet registrator')) {
                 columnIndexes.judr = index;
@@ -97,6 +101,7 @@
             const cells = thirdRow.querySelectorAll('td');
             numarInregistrare = cells[columnIndexes.numarInregistrare]?.innerText.trim() || 'Necunoscut';
             dataInregistrare = cells[columnIndexes.dataInregistrare]?.innerText.trim() || 'Necunoscut';
+            judcerere = cells[columnIndexes.judcerere]?.innerText.trim() || 'Necunoscut';
             registrator = cells[columnIndexes.registrator]?.innerText.trim() || 'Necunoscut';
             judr = cells[columnIndexes.judr]?.innerText.trim() || 'Necunoscut';
             operator = cells[columnIndexes.operator]?.innerText.trim() || 'Necunoscut';
@@ -112,6 +117,7 @@
         //usernameExpeditor = 'adriana.mirea';
         //numarInregistrare = '9999999';
         //dataInregistrare = '31.12.2024';
+        //judcerere = 'Arad';
         //registrator = 'alexandra.decean';
         //operator = 'madalina.manda';
         //firma = 'TEST SRL';
@@ -134,7 +140,8 @@
         formContainer.style.left = '30%';
         formContainer.style.backgroundColor = 'white';
         formContainer.style.border = '1px solid #ccc';
-        formContainer.style.padding = '20px';
+        formContainer.style.padding = '30px';
+        formContainer.style.borderRadius = '10px';
         formContainer.style.zIndex = 10000;
         formContainer.style.width = '600px';
         formContainer.style.boxShadow = '0px 4px 10px rgba(0, 0, 0, 0.1)';
@@ -146,9 +153,9 @@
         closeButton.style.top = '10px';
         closeButton.style.right = '15px';
         closeButton.style.cursor = 'pointer';
-        closeButton.style.fontSize = '18px';
-        closeButton.style.fontWeight = 'bold';
-        closeButton.style.color = '#888';
+        closeButton.style.fontSize = '24px';
+        closeButton.style.fontWeight = 'bolder';
+        closeButton.style.color = 'darkred';
 
         closeButton.addEventListener('click', function() {
             document.body.removeChild(formContainer); // Close the form when 'X' is clicked
@@ -165,6 +172,7 @@
         cancelButton.style.fontSize = '16px';
         cancelButton.style.borderRadius = '3px';
         cancelButton.style.zIndex = 1000;
+        cancelButton.style.backgroundColor = 'seashell';
 
         cancelButton.addEventListener('click', function() {
             document.body.removeChild(formContainer); // Close the form when 'Cancel' is clicked
@@ -182,44 +190,103 @@
         let operatorChecked = '';
         let registratorChecked = '';
 
-        if (usernameExpeditor === operator) {
-            operatorChecked = '';
-            registratorChecked = 'checked';
-        } else if (usernameExpeditor === registrator) {
-            operatorChecked = 'checked';
-            registratorChecked = '';
-        }
+
 
         // Append the checkboxes and text area to the form
         const formContent = `
-            <h5>Probleme la dosar Nr. ${numarInregistrare} / ${dataInregistrare}:</h5>
-            <h5>Pentru firma: ${firma}</h5>
+            <h3>Probleme la dosar Nr. <strong>${numarInregistrare}</strong> / <strong>${dataInregistrare}</strong></h3>
+            <h3>Pentru firma: <strong>${firma}</strong> din <strong>${judcerere}</strong></h3>
             <hr>
             <div>
-                <input type="checkbox" id="option1" name="options" value="Semnați încheierea">
-                <label for="option1">Semnați încheierea</label><br>
+                <input type="checkbox" id="option1" name="options" value="Încărcați în opis încheierea semnată">
+                <label for="option1">Încărcați în opis încheierea semnată</label><br>
             </div>
             <div>
-                <input type="checkbox" id="option2" name="options" value="Încărcați încheierea">
-                <label for="option2">Încărcați încheierea</label><br>
+                <input type="checkbox" id="option2" name="options" value="Încărcați în opis încheierea nesemnată">
+                <label for="option2">Încărcați în opis încheierea nesemnată</label><br>
             </div>
             <div>
-                <input type="checkbox" id="option3" name="options" value="Corectați opisul">
-                <label for="option3">Corectați opisul</label><br>
+                <input type="checkbox" id="option3" name="options" value="Corectați opisul cererii de la pagina - la pagina">
+                <label for="option3">Corectați opisul cererii de la pagina - la pagina</label><br>
+            </div>
+            <div>
+                <input type="checkbox" id="option4" name="options" value="Procesați cererea">
+                <label for="option4">Procesați cererea</label><br>
+            </div>
+            <div>
+                <input type="checkbox" id="option5" name="options" value="Soluționați cererea">
+                <label for="option5">Soluționați cererea</label><br>
             </div>
             <br/>
             <div>
                 <label for="messageBox">Alte probleme:</label><br>
-                <textarea id="messageBox" name="message" rows="4" cols="60"></textarea><br><br>
+                <textarea id="messageBox" name="message" rows="4"style="width: 100%;" ></textarea><br><br>
             </div>
             <div>
-                <label>Trimite către:</label><br>
-                <input type="radio" id="operatorRadio" name="role" value="${operator}" ${operatorChecked} ${operatorChecked || registratorChecked ? 'disabled' : ''}>
-                <label for="operatorRadio">Operator - ${operator} (${judo})</label><br>
-                <input type="radio" id="registratorRadio" name="role" value="${registrator}" ${registratorChecked} ${operatorChecked || registratorChecked ? 'disabled' : ''}>
-                <label for="registratorRadio">Registrator - ${registrator} (${judr})</label><br><br>
-            </div>
-            <input type="submit" value="Trimite">
+        <label>Trimite către:</label><br>
+
+        <!-- Operator Checkbox -->
+        <input type="checkbox" id="operatorCheckbox" name="role" value="${operator}" ${operatorChecked} ${usernameExpeditor === operator ? 'disabled' : ''}>
+        <label for="operatorCheckbox">Operator - ${operator} (${judo})</label><br>
+
+        <!-- Registrator Checkbox -->
+        <input type="checkbox" id="registratorCheckbox" name="role" value="${registrator}" ${registratorChecked} ${usernameExpeditor === registrator ? 'disabled' : ''}>
+        <label for="registratorCheckbox">Registrator - ${registrator} (${judr})</label><br>
+
+        <!-- ORCT Checkbox with Dropdown -->
+        <div style="display: flex; align-items: center;">
+            <input type="checkbox" id="orctCheckbox" name="role" value="ORCT">
+            <label for="orctCheckbox">&nbsp;ORCT</label>
+
+            <!-- Dropdown next to ORCT checkbox -->
+            <select id="countySelect" style="margin-left: 10px;" disabled>
+                <option value="orcab@ab.onrc.ro" ${judcerere === 'Alba' ? 'selected' : ''}>Alba</option>
+                <option value="orcar@ar.onrc.ro" ${judcerere === 'Arad' ? 'selected' : ''}>Arad</option>
+                <option value="orcag@ag.onrc.ro" ${judcerere === 'Argeş' ? 'selected' : ''}>Argeş</option>
+                <option value="orcbc@bc.onrc.ro" ${judcerere === 'Bacău' ? 'selected' : ''}>Bacău</option>
+                <option value="orcbh@bh.onrc.ro" ${judcerere === 'Bihor' ? 'selected' : ''}>Bihor</option>
+                <option value="orcbn@bn.onrc.ro" ${judcerere === 'Bistriţa-Năsăud' ? 'selected' : ''}>Bistriţa-Năsăud</option>
+                <option value="orcbt@bt.onrc.ro" ${judcerere === 'Botoşani' ? 'selected' : ''}>Botoşani</option>
+                <option value="orcbv@bv.onrc.ro" ${judcerere === 'Braşov' ? 'selected' : ''}>Braşov</option>
+                <option value="orcbr@br.onrc.ro" ${judcerere === 'Brăila' ? 'selected' : ''}>Brăila</option>
+                <option value="orcb@b.onrc.ro" ${judcerere === 'Bucureşti' ? 'selected' : ''}>Bucureşti</option>
+                <option value="orcbz@bz.onrc.ro" ${judcerere === 'Buzău' ? 'selected' : ''}>Buzău</option>
+                <option value="orccs@cs.onrc.ro" ${judcerere === 'Caraş-Severin' ? 'selected' : ''}>Caraş-Severin</option>
+                <option value="orccj@cj.onrc.ro" ${judcerere === 'Cluj' ? 'selected' : ''}>Cluj</option>
+                <option value="orcct@ct.onrc.ro" ${judcerere === 'Constanţa' ? 'selected' : ''}>Constanţa</option>
+                <option value="orccv@cv.onrc.ro" ${judcerere === 'Covasna' ? 'selected' : ''}>Covasna</option>
+                <option value="orccl@cl.onrc.ro" ${judcerere === 'Călăraşi' ? 'selected' : ''}>Călăraşi</option>
+                <option value="orcdj@dj.onrc.ro" ${judcerere === 'Dolj' ? 'selected' : ''}>Dolj</option>
+                <option value="orcdb@db.onrc.ro" ${judcerere === 'Dâmboviţa' ? 'selected' : ''}>Dâmboviţa</option>
+                <option value="orcgl@gl.onrc.ro" ${judcerere === 'Galați' ? 'selected' : ''}>Galați</option>
+                <option value="orcgr@gr.onrc.ro" ${judcerere === 'Giurgiu' ? 'selected' : ''}>Giurgiu</option>
+                <option value="orcgj@gj.onrc.ro" ${judcerere === 'Gorj' ? 'selected' : ''}>Gorj</option>
+                <option value="orchr@hr.onrc.ro" ${judcerere === 'Harghita' ? 'selected' : ''}>Harghita</option>
+                <option value="orchd@hd.onrc.ro" ${judcerere === 'Hunedoara' ? 'selected' : ''}>Hunedoara</option>
+                <option value="orcil@il.onrc.ro" ${judcerere === 'Ialomiţa' ? 'selected' : ''}>Ialomiţa</option>
+                <option value="orcis@is.onrc.ro" ${judcerere === 'Iaşi' ? 'selected' : ''}>Iaşi</option>
+                <option value="orcif@if.onrc.ro" ${judcerere === 'Ilfov' ? 'selected' : ''}>Ilfov</option>
+                <option value="orcmm@mm.onrc.ro" ${judcerere === 'Maramureş' ? 'selected' : ''}>Maramureş</option>
+                <option value="orcmh@mh.onrc.ro" ${judcerere === 'Mehedinţi' ? 'selected' : ''}>Mehedinţi</option>
+                <option value="orcms@ms.onrc.ro" ${judcerere === 'Mureş' ? 'selected' : ''}>Mureş</option>
+                <option value="orcnt@nt.onrc.ro" ${judcerere === 'Neamţ' ? 'selected' : ''}>Neamţ</option>
+                <option value="orcot@ot.onrc.ro" ${judcerere === 'Olt' ? 'selected' : ''}>Olt</option>
+                <option value="orcph@ph.onrc.ro" ${judcerere === 'Prahova' ? 'selected' : ''}>Prahova</option>
+                <option value="orcsm@sm.onrc.ro" ${judcerere === 'Satu Mare' ? 'selected' : ''}>Satu Mare</option>
+                <option value="orcsb@sb.onrc.ro" ${judcerere === 'Sibiu' ? 'selected' : ''}>Sibiu</option>
+                <option value="orcsv@sv.onrc.ro" ${judcerere === 'Suceava' ? 'selected' : ''}>Suceava</option>
+                <option value="orcsj@sj.onrc.ro" ${judcerere === 'Sălaj' ? 'selected' : ''}>Sălaj</option>
+                <option value="orctr@tr.onrc.ro" ${judcerere === 'Teleorman' ? 'selected' : ''}>Teleorman</option>
+                <option value="orctm@tm.onrc.ro" ${judcerere === 'Timiş' ? 'selected' : ''}>Timiş</option>
+                <option value="orctl@tl.onrc.ro" ${judcerere === 'Tulcea' ? 'selected' : ''}>Tulcea</option>
+                <option value="orcvs@vs.onrc.ro" ${judcerere === 'Vaslui' ? 'selected' : ''}>Vaslui</option>
+                <option value="orcvn@vn.onrc.ro" ${judcerere === 'Vrancea' ? 'selected' : ''}>Vrancea</option>
+                <option value="orcvl@vl.onrc.ro" ${judcerere === 'Vâlcea' ? 'selected' : ''}>Vâlcea</option>
+            </select>
+        </div>
+    </div>
+            <br/><br/>
+            <input type="submit" value="Trimite" style="position: absolute; bottom: 20px; background-color: honeydew; left: 20px; padding: 2px 10px; border: 1px solid #AAA; font-size: 16px; border-radius: 3px; z-index: 1000;">
         `;
 
         // Append the form content to the form
@@ -230,6 +297,22 @@
 
         // Add the form container to the body
         document.body.appendChild(formContainer);
+
+        document.getElementById('orctCheckbox').addEventListener('change', function() {
+        const countySelect = document.getElementById('countySelect');
+        countySelect.disabled = !this.checked; // Enable dropdown when ORCT is checked
+        });
+
+        // Ensure that only one of the checkboxes (operator or registrator) is checked at a time
+        const operatorCheckbox = document.getElementById('operatorCheckbox');
+        const registratorCheckbox = document.getElementById('registratorCheckbox');
+
+        // Logic to disable operator or registrator based on the current user's role
+        if (usernameExpeditor === operator) {
+            operatorCheckbox.disabled = true; // Disable operator checkbox if user is operator
+        } else if (usernameExpeditor === registrator) {
+            registratorCheckbox.disabled = true; // Disable registrator checkbox if user is registrator
+        }
 
         // Add event listener for form submission
         document.getElementById('myForm').addEventListener('submit', function(event) {
@@ -265,13 +348,29 @@
                 alert('Bifează un mesaj sau scrie problema în caseta text!');
                 return; // Prevent form submission
             }
-                        // Check if a radio button is selected
+            // Check if a destination is selected
             if (!document.querySelector('input[name="role"]:checked')) {
-                alert('Alege un destinatar pentru mesaj (Registrator sau Operator)!');
+                alert('Alege un destinatar pentru mesaj!');
                 return; // Prevent form submission
             }
 
-            usernameDestinatar = role
+            //usernameDestinatar = role
+
+            // Get elements
+            const operatorCheckbox = document.getElementById('operatorCheckbox');
+            const registratorCheckbox = document.getElementById('registratorCheckbox');
+            const orctCheckbox = document.getElementById('orctCheckbox');
+            const countySelect = document.getElementById('countySelect');
+
+            if (orctCheckbox.checked) {mailJudet = countySelect.value;} // Get the selected value from dropdown}
+
+            if (operatorCheckbox.checked) {
+                usernameDestinatar = operatorCheckbox.value;
+            } else if (registratorCheckbox.checked) {
+                usernameDestinatar = registratorCheckbox.value;
+            } else if (orctCheckbox.checked) {
+                usernameDestinatar = mailJudet;
+            }
 
             // Log the form data
             console.log("Mesaj de trimis:", formData);
@@ -294,7 +393,7 @@
             usernameDestinatar: usernameDestinatar,
             usernameExpeditor: usernameExpeditor,
             formData: formData,
-      })
+        })
 
         console.log("Date transmise: ", dateDeTrimis);
 
@@ -318,7 +417,10 @@
 
                     let emailDestinatar = data.emailUtilizator || 'Introdu adresa de email';
                     let subiect = 'Vă rog remediați cererea: ' + numarInregistrare + ' din ' + dataInregistrare; // Subiectul emailului
-                    let corpEmail = 'Vă rog remediați cererea: ' + numarInregistrare + ' din ' + dataInregistrare + '\nCe are următoarea problemă: ' + formData;;
+                    let corpEmail = 'Vă rog remediați cererea: ' + numarInregistrare + ' din ' + dataInregistrare + '\nCe are următoarea problemă: ' + formData;
+                    if (mailJudet && mailJudet.includes('@') && mailJudet != emailDestinatar) {
+                        emailDestinatar += ',' + mailJudet; // Add mailJudet to the recipients list if valid
+                    }
 
                     // Construim URL-ul de tip mailto
                     let mailtoLink = 'mailto:' + emailDestinatar + '?subject=' + encodeURIComponent(subiect) + '&body=' + encodeURIComponent(corpEmail);
